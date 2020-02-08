e573f21fb5288656f67bbf0f9b390e0a7e74a3b1
fly6022 <2875752791@qq.com>
2020-02-08 17:10:50 +0800

    更新网站

--- gitalk.js
+++ gitalk.js
@@ -1,10 +1,14 @@
+// 『书摘。』评论系统 （使用Gitalk）
+// 下面是配置参数
+
+var title = location.pathname.substr(0, 50); // 如果参数超过50个字符，则自动截取前50个字符，为了防止报错。
 const gitalk = new Gitalk( {
 clientID: '1daf307921f9e762e83f',
 clientSecret: '82f60af1acc31ebdbd173c9974185a3a1f52103e',
-repo: 'book-excerpts.github.io',
+repo: 'book.fly6022.fun',
 owner: 'Book-Excerpts',
 admin: 'fly6022',
-id: location.pathname,      // Ensure uniqueness and length less than 50
+id: title,      // Ensure uniqueness and length less than 50
 distractionFreeMode: false  // Facebook-like distraction free mode
 })
 
