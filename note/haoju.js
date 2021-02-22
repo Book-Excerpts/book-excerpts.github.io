9958e782c89d00417fdd9f15c48080c7ea76c2de
fly6022 <2875752791@qq.com>
2021-02-22 19:23:22 +0800

    date()

--- note/haoju.js
+++ note/haoju.js
@@ -5,12 +5,16 @@
 
 function gethaojudata() {  
     
-    var haojudata = ["https://book.fly6022.fun/note/haoju/1.js","https://book.fly6022.fun/note/haoju/2.js"]; // 枚举好句数据源
+    var haojudata = ["src/haoju/1.js","src/haoju/2.js"]; // 枚举好句数据源
 
     var js = document.createElement("script"); 
      js.src = haojudata[Math.floor(Math.random()*2)]; // 生成随机数、寻找枚举对象并填充HTML标签参数
      js.type = "text/javascript";   // 引入的文件类型
      document.getElementsByTagName("body")[0].appendChild(js)  // 引入到HTML标签
+
+}
+
+/*function gethaojucss() { 
 
     var css = document.createElement("link"); 
      css.href = "https://fonts.googleapis.com/css?family=Noto+Serif+SC:200,300,400,500,600,700,900";
@@ -18,5 +22,8 @@ function gethaojudata() {
      document.getElementsByTagName("head")[0].appendChild(css)
 
 }
+*/
 
-setTimeout("gethaojudata()","0"); // 调用函数
+setTimeout("gethaojudata()","0"); // 调用数据源
+
+/* setTimeout("gethaojucss()","0"); // 调用CSS */
